cde35bc1c5ca70dc8599b735b6905288d345474d
AdamJChen <adam.chen9909@gmail.com>
2017-06-12 22:04:51 -0400

    add accuracy

--- leNet.py
+++ leNet.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from keras.datasets import cifar10
 from networks import Conv_net
+from networks import Utes
 
 
 # get CIFAR10 data
@@ -22,14 +23,17 @@ strides = {'c' : [1, 1 ,1 ,1], 'p' : [1, 2, 2, 1]}
 pool_win_size = [1, 2, 2, 1]
 n_cells = 500
 n_classes = 10
-learning_rate = 0.01
+learning_rate = 0.001
 batch_size = 100
 n_epochs = 20
 
 
 # LeNet Model
 def leNet(X):
-	"""Defines the LeNet architecture for image recognition. Returns logits for prediction or training"""
+	"""Defines the LeNet architecture for image recognition. 
+	   If training = True then expecting training set.
+	   If training = False expecting test or validation set.
+	   Returns logits for prediction or training"""
 	with tf.variable_scope('layer1') as scope:
 		conv1 = Conv_net.add_conv(X = X, shape = shapes['c1'], strides = strides['c'])
 		pool1 = Conv_net.add_max_pool(X = conv1, window_size = pool_win_size, strides = strides['p'])
@@ -38,21 +42,14 @@ def leNet(X):
 		conv2 = Conv_net.add_conv(X = pool1, shape = shapes['c2'], strides = strides['c'])
 		pool2 = Conv_net.add_max_pool(X = conv2, window_size = pool_win_size , strides = strides['p'])
 
-	#FIXed to BATCH SIZE
-	flat = tf.reshape(pool2, (batch_size, -1)) # flattens volume into plane for feed forward layers, X.shape[0] is none
+	# flattens volume into plane for feed forward layers
+		flat = tf.reshape(pool2, (batch_size, -1)) 
 
 	with tf.variable_scope('layer3') as scope:
-		fc = Conv_net.add_fc( X = flat, n_in = 3200, n_out = n_cells, act_func = True) # n_in needs to be int
+		fc = Conv_net.add_fc( X = flat, n_in = 3200, n_out = n_cells, act_func = True) # n_in = 8*8*50
 
 	with tf.variable_scope('layer4') as scope:
 		return Conv_net.add_fc( X = fc, n_in = n_cells, n_out = n_classes, act_func = False)
-
-
-def accuracy(labels, logits, n_classes):
-	"""Returns the accuracy of given examples"""
-	predictions = tf.nn.softmax(logits)
-	one_hot_labels = tf.one_hot(indices = labels, depth = n_classes)
-	return tf.metrics.accuracy(labels = one_hot_labels, predictions = predictions)
 
 
 # graph construction
@@ -60,19 +57,19 @@ graph = tf.Graph()
 
 with graph.as_default():
 	#data operations
-	X = tf.placeholder(dtype = tf.float32, shape = (None,image_width, image_width, image_depth))
-	y = tf.placeholder(tf.int32, shape=(None,))
+	train_data = tf.placeholder(dtype = tf.float32, shape = (None,image_width, image_width, image_depth))
+	train_labels = tf.placeholder(tf.int32, shape=(None,))
 
 	# train model
-	model = leNet(X)
-	element_wise_cost = tf.nn.sparse_softmax_cross_entropy_with_logits(logits = model, labels = y)
-	cost = tf.reduce_mean(element_wise_cost)	
-	optimizer = tf.train.GradientDescentOptimizer(learning_rate = learning_rate)
+	train_model = leNet(train_data)
+	element_wise_cost = tf.nn.sparse_softmax_cross_entropy_with_logits(logits = train_model, labels = train_labels)
+	cost = tf.reduce_mean(element_wise_cost)
+	optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
 	train = optimizer.minimize(cost)
-	
-	# evaluate accuracy
-	train_accuracy = accuracy(y, model, n_classes =n_classes)
-	test_accuracy = accuracy(y, model, n_classes =n_classes)
+
+	# training predictions and accuracy
+	train_predictions = Utes.predictions(logits = train_model)
+	train_accuracy = Utes.accuracy(labels = train_labels, predictions = train_predictions, n_classes = n_classes)
 
 
 # graph  execution
@@ -86,11 +83,13 @@ with tf.Session(graph = graph) as sess:
 			# define offset and end of batch
 			offset = (batch * batch_size)
 			end_batch = offset + batch_size
+			
 			# get batch
 			batch_data = X_train[offset: end_batch,:, :, :]
 			batch_labels = y_train[offset: end_batch]
-		# batch_data = X_train[0:5]
-		# batch_labels = y_train[0:5]
-			feed_dict = {X: batch_data, y: batch_labels}
-			_, cost_value = sess.run([train, cost], feed_dict = feed_dict)
-		print 'batch number: %d cost: %f' % (batch, cost_value)
+			
+			# feed and run training
+			train_feed_dict = {train_data: batch_data, train_labels: batch_labels}
+			_, cost_value, train_acc_value = sess.run([train, cost, train_accuracy], feed_dict =train_feed_dict)
+			print 'batch number: %d cost: %.2f accuracy: %.2f%%' % (batch, cost_value, train_acc_value * 100)
+
